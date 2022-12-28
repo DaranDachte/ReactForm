a6b276e3f5a8d840f1db8c736ee6177d3fa6b870
Daran Dachte <darandachte@gmail.com>
2022-12-28 18:45:29 +0100

    first

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,51 @@
+import { useForm } from "react-hook-form";
+import { schema } from "./validation";
+import { yupResolver } from "@hookform/resolvers/yup";
+import styles from "./Form.module.css";
+
+const Form = (props) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(schema) });
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <div className={styles.container}>
+      <form onSubmit={handleSubmit(onSubmit)} className={styles.wrapper}>
+        <input
+          className={styles.input}
+          type="text"
+          placeholder="Enter your name"
+          {...register("name")}
+        />
+        {!!errors.name && <p className={styles.error}>{errors.name?.message}</p>}
+        <select
+          className={styles.selector}
+          placeholder="Choose topic of question"
+          {...register("topic")}
+        >
+          <option>I have depression</option>
+          <option>Question for a lawyer</option>
+          <option>Social help</option>
+          <option>Work in Germany</option>
+          <option>Education</option>
+          <option>Medicine and health</option>
+          <option>Violence in family</option>
+          <option selected>Other</option>
+        </select>
+        <textarea className={styles.textarea} {...register("text")} />
+        {!!errors.text && <p className={styles.error}>{errors.text?.message}</p>}
+        <button className={styles.btn} type="submit">
+          Send
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
