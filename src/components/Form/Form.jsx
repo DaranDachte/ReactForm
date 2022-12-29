681d1a7f20717779b43bde7f40e6c304ac3ccaa4
Daran Dachte <darandachte@gmail.com>
2022-12-29 22:39:17 +0100

    29 dec

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -15,36 +15,46 @@ const Form = (props) => {
   };
 
   return (
-    <div className={styles.container}>
-      <form onSubmit={handleSubmit(onSubmit)} className={styles.wrapper}>
-        <input
-          className={styles.input}
-          type="text"
-          placeholder="Enter your name"
-          {...register("name")}
-        />
-        {!!errors.name && <p className={styles.error}>{errors.name?.message}</p>}
-        <select
-          className={styles.selector}
-          placeholder="Choose topic of question"
-          {...register("topic")}
-        >
-          <option>I have depression</option>
-          <option>Question for a lawyer</option>
-          <option>Social help</option>
-          <option>Work in Germany</option>
-          <option>Education</option>
-          <option>Medicine and health</option>
-          <option>Violence in family</option>
-          <option selected>Other</option>
-        </select>
-        <textarea className={styles.textarea} {...register("text")} />
-        {!!errors.text && <p className={styles.error}>{errors.text?.message}</p>}
-        <button className={styles.btn} type="submit">
-          Send
-        </button>
-      </form>
-    </div>
+    <>
+      <div className={styles.container}>
+        
+        <form onSubmit={handleSubmit(onSubmit)} className={styles.wrapper}>
+          <h4>Name:</h4>
+          <input
+            className={styles.input}
+            type="text"
+            placeholder="Enter your name"
+            {...register("name")}
+          />
+          {!!errors.name && (
+            <p className={styles.error}>{errors.name?.message}</p>
+          )}
+          <h4>Topic of question:</h4>
+          <select
+            className={styles.selector}
+            placeholder="Choose topic of question"
+            {...register("topic")}
+          >
+            <option>I have depression</option>
+            <option>Question for a lawyer</option>
+            <option>Social help</option>
+            <option>Work in Germany</option>
+            <option>Education</option>
+            <option>Medicine and health</option>
+            <option>Violence in family</option>
+            <option selected>Other</option>
+          </select>
+          <h4>Ask your question here:</h4>
+          <textarea className={styles.textarea} {...register("text")} />
+          {!!errors.text && (
+            <p className={styles.error}>{errors.text?.message}</p>
+          )}
+          <button className={styles.btn} type="submit">
+            Send
+          </button>
+        </form>
+      </div>
+    </>
   );
 };
 
