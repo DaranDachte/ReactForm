29a4efcabe421c111b9b78c3e48cbd460254402b
Daran Dachte <darandachte@gmail.com>
2022-12-30 16:41:42 +0100

    with server2

--- server.js
+++ server.js
@@ -9,8 +9,6 @@ const app = express();
 
 app.use(express.json());
 
-
-
 mongoose
   .connect(
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`
@@ -22,6 +20,6 @@ mongoose
     console.log("an error occurred while connecting ot the db", error);
   });
 
-  app.listen(process.env.PORT || 3001, (req, res) => {
-    console.log(`the server listening on port ${PORT}`);
-  });
+app.listen(process.env.PORT || 3001, (req, res) => {
+  console.log(`the server listening on port ${PORT}`);
+});
